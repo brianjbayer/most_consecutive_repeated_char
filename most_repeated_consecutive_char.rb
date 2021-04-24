72375a3d23428509bb95e5c359adb37b6ebe8404
brianjbayer <brianjbayer@gmail.com>
2021-04-24 11:46:48 -0400

    Refactor for basic code quality...
     - Separate the presentation from the find
     - Specify and handle unspecified behavior
       of obvious edge-case of no consecutive
       repeating characters at implementation
       level

--- most_repeated_consecutive_char.rb
+++ most_repeated_consecutive_char.rb
@@ -1,7 +1,13 @@
 # This class finds the most repeated consecutive character in a string
 class MostRepeatedConsecutiveChar
+  def self.print(str)
+    max_char_and_count = find(str)
+    "#{max_char_and_count[0]}: #{max_char_and_count[1]}"
+  end
+
   def self.find(str)
     vector_max_char_count = str.chars.chunk{|c| c}.map{|c, cc| [c, cc.length]}.max_by{|k,v| v}
-    "#{vector_max_char_count[0]}: #{vector_max_char_count[1]}"
+    return nil if vector_max_char_count[1] < 2
+    vector_max_char_count
   end
 end
--- spec/most_consecutive_repeated_char_spec.rb
+++ spec/most_consecutive_repeated_char_spec.rb
@@ -2,7 +2,17 @@
 require_relative '../most_repeated_consecutive_char'
 
 RSpec.describe 'MostRepeatedConsecutiveChar' do
+  describe 'print' do
+    it { expect(MostRepeatedConsecutiveChar.print('ANKRURRR')).to eql 'R: 3' }
+  end
+
   describe 'find' do
-    it { expect(MostRepeatedConsecutiveChar.find('ANKRURRR')).to eql 'R: 3' }
+    let(:mult_repeats_of_same_char)  { '10011000 ' }
+    let(:max_repeated_count)  { ["0", 3] }
+
+    let (:no_repeat_chars) { '!@#$%^&*()_='}
+
+    it { expect(MostRepeatedConsecutiveChar.find(mult_repeats_of_same_char)).to eql max_repeated_count }
+    it { expect(MostRepeatedConsecutiveChar.find(no_repeat_chars)).to be nil }
   end
 end
