52dcfc5fb1afc27afcad10fc8cff98d7ecc53aec
brianjbayer <brianjbayer@gmail.com>
2021-04-24 11:10:27 -0400

    MVP passing acceptance criteria

--- most_repeated_consecutive_char.rb
+++ most_repeated_consecutive_char.rb
@@ -1,6 +1,7 @@
 # This class finds the most repeated consecutive character in a string
 class MostRepeatedConsecutiveChar
   def self.find(str)
-    # TODO
+    vector_max_char_count = str.chars.chunk{|c| c}.map{|c, cc| [c, cc.length]}.max_by{|k,v| v}
+    "#{vector_max_char_count[0]}: #{vector_max_char_count[1]}"
   end
 end
